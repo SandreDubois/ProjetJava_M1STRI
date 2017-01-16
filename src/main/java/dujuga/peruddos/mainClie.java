734038e1a9a1b0dd2afed53302e4762edbbd8122
Agardavoir <gardavoiralexis@gmail.com>
2017-01-17 00:33:24 +0100

    PER-1102, PER-1103
    Le client envoie actuellement un chiffre (à changer), le serveur peut
    seulement recevoir un client et recevoir un message.

---
+++ src/main/java/dujuga/peruddos/mainClie.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dujuga.peruddos;
+
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+/**
+ *
+ * @author Alexis
+ */
+public class mainClie {
+
+    private String adresse = "127.0.0.1";
+    
+    private void envoieInt(Socket sockService, int chiffre){
+        try{
+            DataOutputStream oStream = new DataOutputStream(sockService.getOutputStream());
+            oStream.writeInt(chiffre);
+        }
+        catch(IOException ioe){
+            System.out.println("Erreur lors de l'envoie d'un chiffre : " + ioe.getMessage());
+        }
+              
+    }
+    
+    private void gestionSocket(){
+        Socket sock = null;
+        try{
+            sock = new Socket(adresse, 18000);
+        }
+        catch(IOException ioe){
+            System.out.println("Erreur lors de la connexion : " + ioe.getMessage());
+        }
+        
+        envoieInt(sock, 4);
+    }
+    public static void main(String[] args) {
+        mainClie mainClie = new mainClie();
+        System.out.println("Début client.");
+        mainClie.gestionSocket();
+    }
+    
+}
