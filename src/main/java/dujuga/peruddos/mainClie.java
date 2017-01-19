1b0170442846e2f0193b2d174aa9f7d3a3d21f2d
Sandre Dubois <sandre.dubois@hotmail.fr>
2017-01-19 11:42:57 +0100

    Ajout message d'accueil

--- src/main/java/dujuga/peruddos/mainClie.java
+++ src/main/java/dujuga/peruddos/mainClie.java
@@ -20,19 +20,22 @@ import java.util.Scanner;
 public class mainClie {
     /* Créer une méthode permettant le changement de l'adresse par l'utilisateur. */
     private String adresse = "127.0.0.1";
+    private String pseudostr;
        
     /* Envoie du pseudo du client  */
     private void envoiePseudo(Socket sockService){
         try{
             Scanner pseudo = new Scanner(System.in);
-            System.out.println("Pseudo : ");
-            String str = pseudo.nextLine();
+            System.out.println("Veuillez rentrer votre pseudo : ");
+            pseudostr = pseudo.nextLine();
             DataOutputStream oStream = new DataOutputStream(sockService.getOutputStream());
-            oStream.writeUTF(str);
+            oStream.writeUTF(pseudostr);
         } catch(IOException ioe){
             System.out.println("Erreur lors de l'envoie du pseudo : " + ioe.getMessage());
         }
     }
+    
+    
     
     /* Envoie de l'IP et du port du client  */
     private void envoieIP(Socket sockService){
@@ -46,11 +49,19 @@ public class mainClie {
         }
     }
     
+    private void accueil(){
+        System.out.println("++++++++++++++++++++++++++++++++++++++++");        
+        System.out.println("+++++++++++++++ PERUDDOS +++++++++++++++");
+        System.out.println("++++++++++++++++++++++++++++++++++++++++");        
+        System.out.println("            Bonjour "+pseudostr+ "!");
+    }
+    
     /* Connecte le client au serveur */
     private void gestionSocket() {
         Socket sock = null;
         try{
             sock = new Socket(adresse, 18000);
+            System.out.println("Connexion réussi au serveur.");
         }
         catch(IOException ioe){
             System.out.println("Erreur lors de la connexion : " + ioe.getMessage());
@@ -58,6 +69,7 @@ public class mainClie {
         
         envoiePseudo(sock);
         envoieIP(sock);
+        accueil();
     }
     
     public static void main(String[] args) {
